27fed2e8153395a8a640b575a818b2b3cdc9016b
liztrenholme <liztrenholme@gmail.com>
2026-06-29 17:52:48 -0400

    adds footer and navbar components

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './footer.css'
+
+function Footer(props) {
+  return (
+    <footer className="footer-container">
+        This site is not affiliated with Cleveland State University or IdeaStream Public Media.
+    </footer>
+  )
+}
+
+export default Footer
