757fae1b41e26d32d0c7e86ad2d82122085b6f38
liztrenholme <liztrenholme@gmail.com>
2026-08-11 17:33:00 -0400

    events page buildout

---
+++ src/assets/files/events.js
@@ -0,0 +1,64 @@
+import img from '../fliers/CPP.jpeg'
+
+export const eventsList = {
+    "August": [
+        {
+            "date": "Sat.–Sun. 8/15-16",
+            "eventName": "XCSB table at Cleveland Pagan Pride",
+            "time": "Sat. 12pm-10pm, Sun. 12pm-6pm",
+            "location": "730 Broadway Ave., Bedford",
+            "description": "Come say hi to us at Cleveland Pagan Pride!",
+            "image": img,
+            "eventId": Math.random()
+        }
+    ],
+    "September": [
+        {
+            "date": "Sept. 12, 2026",
+            "eventName": "XCSB table at Waterloo Arts Fest",
+            "time": "12pm-7pm",
+            "location": "Outside our future studio! 15721 Waterloo Rd., Cleveland",
+            "description": "Food, vendors, music, bands",
+            "image": "",
+            "eventId": Math.random()
+        },
+        {
+            "date": "Sept. 12, 2026",
+            "eventName": "XCSB table at LOVE FEST",
+            "time": "12pm-10pm",
+            "location": "Reithoffer's Art, Spirits & Entertainment at 17711 Ravenna Rd., Chagrin Falls",
+            "description": "In collaboration with COOL AND GROOVY, INC",
+            "image": "",
+            "eventId": Math.random()
+        },
+        {
+            "date": "Sept. 19, 2026",
+            "eventName": "XCSB table at Tremont Arts Fest",
+            "time": "12pm-7pm",
+            "location": "Lincoln Park, Cleveland",
+            "description": "Celebrating culture and artistic diversity of Tremont and the greater Cleveland area. More info <a href='https://experiencetremont.com/featured-events/tremont-arts-cultural-festival/'>here</a>",
+            "image": "",
+            "eventId": Math.random()
+        },
+        {
+            "date": "Sept. 26, 2026",
+            "eventName": "XCSB FLEA MARKET",
+            "time": "10am-3pm",
+            "location": "Beachland Ballroom & Tavern, 15711 Waterloo Rd, Cleveland",
+            "description": "Spend the day browsing a wide variety of vendors and listen to some of our DJ’s spinning records. $10 Donation at the door. Would you like to be a vendor? Click <a href='https://docs.google.com/forms/d/1RZ_4QM06Hpox3IUHP9_MMFD-yfq6NPYDT4AChc9j8NE/viewform?edit_requested=true'>here</a> to apply!",
+            "image": "",
+            "eventId": Math.random()
+        }
+    ],
+    "November": [
+        {
+            "date": "November 30, 2026",
+            "eventName": "XCSB DJ MUSIC TRIVIA NIGHT",
+            "time": "Starts at 8pm",
+            "location": "Happy Dog 5801 Detroit Ave., Cleveland",
+            "description": "XCSB DJ MUSIC TRIVIA NIGHT with XCSB DJ’s & Local DJ’s putting their music knowledge to the test!",
+            "image": "",
+            "eventId": Math.random()
+        }
+    ],
+}
--- src/routes/Events/index.jsx
+++ src/routes/Events/index.jsx
@@ -1,10 +1,70 @@
-import { useState } from 'react'
+import { useState, useEffect, useEffectEvent } from 'react'
 import './events.css'
+import { eventsList } from '../../assets/files/events'
 
 function Events(props) {
+  const [isExpanded, setIsExpanded] = useState({});
+  const [isEventExpanded, setIsEventExpanded] = useState({});
+
+  const handleSetMonthExpanded = (month, expanded) => {
+    setIsExpanded(prev => ({
+      ...prev,
+      [month]: expanded
+    }));
+  }
+
+  const handleSetEventExpanded = (eventId, expanded) => {
+    setIsEventExpanded(prev => ({
+      ...prev,
+      [eventId]: expanded
+    }));
+  }
+
+  const handleSetMonths = useEffectEvent(() => {
+    const updated = {}
+    Object.keys(eventsList).forEach(i => updated[i] = true)
+    setIsExpanded(updated)
+  });
+
+  const handleSetEvents = useEffectEvent(() => {
+    const updated = {}
+    console.log(Object.keys(eventsList).map(m => eventsList[m].map(event => event.eventId)))
+    Object.keys(eventsList).forEach(m => eventsList[m].forEach(event => updated[event.eventId] = false))
+    setIsEventExpanded(updated)
+  });
+
+  useEffect(() => {
+    if (eventsList && Object.keys(eventsList).length && !Object.keys(isExpanded)?.length) {
+      handleSetMonths()
+      handleSetEvents()
+    }
+  }, []);
+
   return (
     <div className="events-container">
-        Events!
+      {Object.keys(eventsList).map(month => <div>
+        <h2 className="month-header" id={`${month}-toggle`}
+          onClick={() => handleSetMonthExpanded(month, !isExpanded[month])}>{month}
+          {!isExpanded[month] ? <i className="arrow right" id={`${month}-arrow-right`}></i> :
+            <i className="arrow down" id={`${month}-arrow-down`}></i>}
+        </h2>
+        {isExpanded[month] ?
+          <div className="month-section" id={`${month}-section`}>
+            {eventsList[month].map(event => <div className="event-blurb"><div><span className="date">{event.date}</span>
+              <p>{event.eventName}</p>
+              <p>{event.location}</p>
+              <p>{event.time}</p>
+              <button className="more-btn" id={`${event.eventId}more`} onClick={() => handleSetEventExpanded(event.eventId, !isEventExpanded[event.eventId])}>More
+                {!isEventExpanded[event.eventId] ? <i className="arrow-more right" id={`${event.eventId}-arrow-right`}></i> :
+                <i className="arrow-more down" id={`${event.eventId}-arrow-down`}></i>}
+                </button>
+              {isEventExpanded[event.eventId] ? <div className="event-description-box" id={`${event.eventId}-description-box`}>{event.description}</div>
+                : null}
+            </div>
+              <img className={`flier-thumbnail ${event.image ? '' : "hidden"}`} src={event.image} alt={event.eventName + ' flier'} />
+            </div>)}
+          </div> : null}
+      </div>)}
     </div>
   )
 }
