0f9f711547cfee9617737b6c959319a6068ecbf9
liztrenholme <liztrenholme@gmail.com>
2026-08-20 14:22:28 -0400

    landing page buildout, styling fixes, updated readme

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -40,7 +40,6 @@ function NavBar(props) {
         setIsOpen(false);
         navigate(link);
     }
-    console.log('width...', width)
     return width && width > 900 ? (
         <nav className="navbar-container" role="navigation">
             <ul className="srt-menu-new">
@@ -66,25 +65,25 @@ function NavBar(props) {
             </ul>
         </nav>
     ) : (
-        <div>
-            <img style={{ width: '40px', height: '30px' }} src={Burger} onClick={() => setIsOpen(!isOpen)} />
+        <nav>
+            <img style={{ width: '40px', height: '30px', zIndex: '9999999999999' }} src={Burger} onClick={() => setIsOpen(!isOpen)} />
             {isOpen ? <ul className="srt-menu-mobile">
-                <li className={window.location.pathname.includes('/about') ? "current nav-item" : 'nav-item'}
+                <li className={window.location.pathname.includes('/about') ? "current" : 'nav-item'}
                     onClick={() => handleCloseAndNavigate(`${import.meta.env.BASE_URL}/about`)}>
                     About</li>
-                <li className={window.location.pathname.includes('/xcsb-programDetail') ? "current nav-item" : 'nav-item'}
+                <li className={window.location.pathname.includes('/xcsb-programDetail') ? "current" : 'nav-item'}
                     onClick={() => handleCloseAndNavigate(`${import.meta.env.BASE_URL}/xcsb-programDetail`)}>
                     Our Programs</li>
-                <li className={window.location.pathname.includes('/xcsb-donate') ? "current nav-item" : 'nav-item'}
+                <li className={window.location.pathname.includes('/xcsb-donate') ? "current" : 'nav-item'}
                     onClick={() => handleCloseAndNavigate(`${import.meta.env.BASE_URL}/xcsb-donate`)}>
                     Donate</li>
-                <li className={window.location.pathname.includes('/xcsb-mediaCoverage') ? "current nav-item " : 'nav-item'}
+                <li className={window.location.pathname.includes('/xcsb-mediaCoverage') ? "current " : 'nav-item'}
                     onClick={() => handleCloseAndNavigate(`${import.meta.env.BASE_URL}/xcsb-mediaCoverage`)}>
                     What Happened?</li>
-                <li className={window.location.pathname.includes('/xcsb-WCSB50Years') ? "current nav-item" : 'nav-item'}
+                <li className={window.location.pathname.includes('/xcsb-WCSB50Years') ? "current" : 'nav-item'}
                     onClick={() => handleCloseAndNavigate(`${import.meta.env.BASE_URL}/xcsb-WCSB50Years`)}>
                     WCSB - 50 Years!</li>
-                <li className={window.location.pathname.includes('/xcsb-events') ? "current nav-item" : 'nav-item'}
+                <li className={window.location.pathname.includes('/xcsb-events') ? "current" : 'nav-item'}
                     onClick={() => handleCloseAndNavigate(`${import.meta.env.BASE_URL}/xcsb-events`)}>
                     Events</li>
                 <li>
@@ -93,7 +92,7 @@ function NavBar(props) {
                     </div>
                 </li>
             </ul> : null}
-        </div>
+        </nav>
     )
 }
 
--- src/routes/Main/index.jsx
+++ src/routes/Main/index.jsx
@@ -1,10 +1,29 @@
 import { useState } from 'react'
 import './main.css'
+import Turntable from '../../assets/turntable.jpg'
 
 function Main(props) {
   return (
     <div className="main-container">
-        <h1>XCSB</h1>
+      <div className="main-lead">
+        {/* <img classname="turntable-image" style={{maxWidth: '50%'}} src={Turntable} alt="turntable" /> */}
+        <div className="img-background">
+          <h1 className="headline">Radio for the community,</h1>
+          <h1 className="headline">by the community.</h1>
+        </div>
+        <div className="buttons-container">
+          <h1>
+            <a href="https://xcsb.live/public/xcsb-cleveland" onclick="openPlayer(event, this.href)" className="buttonlinkwide">
+              Listen Now!
+            </a>
+          </h1>
+          <h1>
+            <a href="https://square.link/u/Eu7UreAU" className="buttonlinkwide" target="_blank">
+            Donate
+            </a>
+            </h1>
+        </div>
+      </div>
     </div>
   )
 }
